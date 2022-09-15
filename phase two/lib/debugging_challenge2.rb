518249250caa4c66ece85e676448b5cf7af2045e
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-09-15 12:03:57 +0100

    Complete debugging challenges

---
+++ phase two/lib/debugging_challenge2.rb
@@ -0,0 +1,25 @@
+def get_most_common_letter(text)
+    counter = Hash.new(0)
+    text.gsub(/\s+/, "").chars.each do |char|
+      counter[char] += 1
+    end
+    counter.to_a.sort_by { |k, v| -v }[0][0]
+  end
+  
+
+  get_most_common_letter("the roof, the roof, the roof is on fire!")
+=begin
+ORIGINAL
+  def get_most_common_letter(text)
+    counter = Hash.new(0)
+    text.chars.each do |char|
+      counter[char] += 1
+    end
+    counter.to_a.sort_by { |k, v| v }[0][0]
+  end
+  =end
+  
+  # Intended output:
+  # 
+  # > get_most_common_letter("the roof, the roof, the roof is on fire!")
+  # => "o"
