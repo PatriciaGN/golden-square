b497a522e2d3c550540a4ca2bb8330b9d4a45b0e
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-09-15 12:27:15 +0100

    Modify count_words function to pass test

--- phase two/lib/diary_entry_class.rb
+++ phase two/lib/diary_entry_class.rb
@@ -14,7 +14,7 @@ class DiaryEntry
     end
   
     def count_words
-      # Returns the number of words in the contents as an integer
+      @contents.split(" ").count
     end
   
     def reading_time(wpm) # wpm is an integer representing the number of words the
