d307bf8b101cad4fa4e7358446f8c6d5c391462b
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-09-15 14:35:19 +0100

    Add tests for reading_chunk method and change method to pass

--- phase two/lib/diary_entry_class.rb
+++ phase two/lib/diary_entry_class.rb
@@ -3,6 +3,7 @@ class DiaryEntry
     def initialize(title, contents) # title, contents are strings
       @title = title
       @contents = contents
+      @counter = 0
     end
   
     def title
@@ -14,17 +15,28 @@ class DiaryEntry
     end
   
     def count_words
-      @contents.split(" ").count
+        @contents.split(" ").count
     end
   
     def reading_time(wpm) # wpm is an integer representing the number of words the
-      total_words = count_words 
-      total_words / wpm                # user can read per minute words / reading_time(minutes) = wpm
+      (count_words / wpm.to_f).ceil                # user can read per minute words / reading_time(minutes) = wpm
       # Returns an integer representing an estimate of the reading time in minutes
       # for the contents at the given wpm.
     end
   
-    def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
+    def reading_chunk(wpm, minutes)
+        total_words = wpm * minutes
+        last_word = @counter + total_words 
+        chunk = @contents.split[@counter...last_word]
+        if last_word >= count_words
+            @counter = 0
+        else
+            @counter = last_word
+        end
+        return chunk.join(" ")
+
+        
+        # `wpm` is an integer representing the number
                                     # of words the user can read per minute
                                     # `minutes` is an integer representing the
                                     # number of minutes the user has to read
@@ -34,4 +46,9 @@ class DiaryEntry
       # what has already been read, until the contents is fully read.
       # The next call after that it should restart from the beginning.
     end
+
+
   end
+  my_diary = DiaryEntry.new(nil, "one two three four five six seven eight nine ten")
+  puts my_diary.reading_chunk(5, 3)
+  puts my_diary.reading_chunk(1, 2)
--- phase two/spec/diary_entry_class_spec.rb
+++ phase two/spec/diary_entry_class_spec.rb
@@ -28,6 +28,45 @@ RSpec.describe DiaryEntry do
           my_diary = DiaryEntry.new(nil, "")
           expect(my_diary.reading_time(200)).to eq 0
        end
+
+       it "it returns the number of minutes it takes to read a text rounded up" do
+          my_diary = DiaryEntry.new(nil, "one")
+          expect(my_diary.reading_time(200)).to eq 1
+       end
+
+       it "it returns the number of minutes it takes to read a text rounded up" do
+          my_diary = DiaryEntry.new(nil, "one " * 200)
+          expect(my_diary.reading_time(200)).to eq 1
+       end
+
+       it "it returns the number of minutes it takes to read a text rounded up" do
+          my_diary = DiaryEntry.new(nil, "one " * 300)
+          expect(my_diary.reading_time(200)).to eq 2
+       end
     end
 
+    describe "#reading_chunk" do
+      it "returns a string with the number of words a user can read read in a given time" do
+        my_diary = DiaryEntry.new(nil, "one two three four five six seven eight nine ten") 
+        expect(my_diary.reading_chunk(2, 3)).to eq "one two three four five six"
+      end
+
+      it "returns the text the user is able to read starting from the previous chunk" do
+        my_diary = DiaryEntry.new(nil, "one two three four five six seven eight nine ten")
+        my_diary.reading_chunk(2, 2) 
+        expect(my_diary.reading_chunk(2, 2)).to eq "five six seven eight"
+      end
+
+      it "stops returning words when it gets to the end of the string" do
+        my_diary = DiaryEntry.new(nil, "one two three four five six seven eight nine ten")
+        expect(my_diary.reading_chunk(5, 3)).to eq "one two three four five six seven eight nine ten"
+      end
+
+      it "starts returning the contents again after it has gotten to the end when called several times" do
+        my_diary = DiaryEntry.new(nil, "one two three four five six seven eight nine ten")
+        my_diary.reading_chunk(5, 3)
+        expect(my_diary.reading_chunk(1, 2)).to eq "one two"
+      end
+
+    end
 end
