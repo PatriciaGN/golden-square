5638c6660440214c42f19615f4bf608d92ebdbc2
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-09-15 12:22:18 +0100

    Add test to check contents function in DearDiary class

--- phase two/spec/diary_entry_class_spec.rb
+++ phase two/spec/diary_entry_class_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe DiaryEntry do
         my_diary = DiaryEntry.new("September", nil)
         expect(my_diary.title).to eq "September"
     end
+
+    it "returns the content as a string" do
+        my_diary = DiaryEntry.new("September", "Dear diary: This is being an amazing month.")
+        expect(my_diary.contents).to eq "Dear diary: This is being an amazing month."
+    end
 end
