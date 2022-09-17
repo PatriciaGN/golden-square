41a0ddce4b0d434cfbdb87e3a7e8cee2b1e78836
Patricia Garcia <patricia@MASTUDE44KCQ6L4.home>
2022-09-17 17:14:32 +0100

    Add tests for count_words method and modify code to pass them

--- phase two/spec/diaryentry_class_spec.rb
+++ phase two/spec/diaryentry_class_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe DiaryEntry do
             expect(new_entry.contents).to eq "my contents"
         end
     end
+
+    describe "#count_words" do 
+        it "Returns 0 if contents are an empty string" do
+          diary_entry = DiaryEntry.new("my_title", "")
+          expect(diary_entry.count_words).to eq 0
+        end
+        
+        it "returns number of words in contents as an integer" do
+          diary_entry = DiaryEntry.new("September", "Dear diary")
+          expect(diary_entry.count_words).to eq 2
+        end
+
+        it "Returns 4 if contents are a 4 words string" do
+          diary_entry = DiaryEntry.new("my_title", "My contents are cool")
+          expect(diary_entry.count_words).to eq 4
+        end
+      end
 end
