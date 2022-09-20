e55eeac57582f4a04689ea3bc867e377202422eb
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-09-20 10:30:54 +0100

    Add second set of integration tests and modify todo and todolist classes to pass them

--- phase two/lib/todo_class.rb
+++ phase two/lib/todo_class.rb
@@ -14,13 +14,12 @@ class Todo
   
     def mark_done!
       fail "That todo was already marked as done" if self.completed == true
-      # @tasks_not_done -= @task
-      # @tasks_done << @task
       self.completed = true
     end
   
     def done?
       # Returns true if the task is done
       # Otherwise, false
+      self.completed
     end
   end
--- phase two/spec/todolist_integration_spec.rb
+++ phase two/spec/todolist_integration_spec.rb
@@ -3,21 +3,31 @@
    
    RSpec.describe "TodoList integration" do
     describe "#add(todo) + #incomplete"  do
-      it "adds a todo to the incomplete list" do
+      it "marks a todo as incomplete" do
         my_todo = Todo.new("Wash up")
         my_list = TodoList.new
         my_list.add(my_todo)
         expect(my_list.incomplete).to eq [my_todo]
       end
 
-      it "returns an empty list if no todos done" do
+      it "returns an empty list if no todos added" do
         my_list = TodoList.new
         my_todo = Todo.new("Wash up")
         expect(my_list.incomplete).to eq []
       end
+ 
+      it "returns several incompleted todos" do
+        my_todo1 = Todo.new("Wash up")
+        my_todo2 = Todo.new("Pet the cat")
+        my_list = TodoList.new
+        my_list.add(my_todo1)
+        my_list.add(my_todo2)
+        expect(my_list.incomplete).to eq [my_todo1, my_todo2]
+      end
+
     end
 
-    describe "#add(todo) + #mark_done! + #complete + #incomplete + #done? + give_up!" do
+    describe "#add(todo) + #mark_done! + #complete" do
       it "returns an error if a todo was already done" do
         my_todo = Todo.new("Wash up")
         my_list = TodoList.new
@@ -33,5 +43,45 @@
         my_todo.mark_done!
         expect(my_list.complete).to eq [my_todo]
       end
+
+      it "marks several todos as done and returns the list" do
+        my_list = TodoList.new
+        my_todo1 = Todo.new("Wash up")
+        my_todo2 = Todo.new("Take mail")
+        my_list.add(my_todo1)
+        my_list.add(my_todo2)
+        my_todo1.mark_done!
+        my_todo2.mark_done!
+        expect(my_list.complete).to eq [my_todo1, my_todo2]
+      end
+    end
+
+    describe "#add(todo) + #mark_done! + #complete + #incomplete + #done? + give_up!" do
+      it "returns true if a todo has been done" do
+        my_list = TodoList.new
+        my_todo = Todo.new("Wash up")
+        my_list.add(my_todo)
+        my_todo.mark_done!
+        expect(my_todo.done?).to eq true
+      end
+
+      it "returns false if a todo hasn't been done" do
+        my_list = TodoList.new
+        my_todo = Todo.new("Wash up")
+        my_list.add(my_todo)
+        expect(my_todo.done?).to eq false
+      end
+
+      # it "marks several todos as done and returns the list" do
+      #   my_list = TodoList.new
+      #   my_todo1 = Todo.new("Wash up")
+      #   my_todo2 = Todo.new("Take mail")
+      #   my_list.add(my_todo1)
+      #   my_list.add(my_todo2)
+      #   my_todo1.mark_done!
+      #   my_todo2.mark_done!
+      #   expect(my_list.complete).to eq [my_todo1, my_todo2]
+      # end
+    
     end
   end
