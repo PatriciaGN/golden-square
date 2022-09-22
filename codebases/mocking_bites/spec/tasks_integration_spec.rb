5d1c4537fa19c5c8a56afe7edaae02b8d0d13243
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-09-22 11:48:10 +0100

    Add task_formatter class and spec, create units tests and modify class to pass

--- codebases/mocking_bites/spec/tasks_integration_spec.rb
+++ codebases/mocking_bites/spec/tasks_integration_spec.rb
@@ -1,5 +1,6 @@
 require "task_list"
 require "task"
+require "task_formatter"
 
 RSpec.describe "tasks integration" do
   it "adds tasks to a list" do
@@ -20,5 +21,15 @@ RSpec.describe "tasks integration" do
     task_1.mark_complete
     task_2.mark_complete
     expect(task_list.all_complete?).to eq true
+  end
+
+  it "formats a list of tasks" do
+    task_list = TaskList.new
+    task_1 = Task.new("Walk the dog")
+    task_2 = Task.new("Walk the cat")
+    task_list.add(task_1)
+    task_list.add(task_2)
+    task_1.mark_complete
+    expect(task_list.all).to eq " - [x] Walk the dog \n -[ ] Walk the cat"
   end
 end
